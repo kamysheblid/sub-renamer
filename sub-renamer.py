61364e1fb111cfc62d96fa87f6595b375f8b9895
kamysheblid <kamysheblid@gmail.com>
2020-09-23 22:04:48 -0400

    integrated the Episode class

--- sub-renamer.py
+++ sub-renamer.py
@@ -1,52 +1,59 @@
 #!/usr/bin/env python3
-import os, re, argparse
-from pathlib import PurePath
+import os, argparse
+from episode import Episode
 
-SUB_EXTS = '(srt|ass|ssa|vtt)'
-VID_EXTS = '(avi|mp4|mpeg|mkv)'
+SUB_EXTS = ['srt','ass','ssa','vtt']
+VID_EXTS = ['avi','mp4','mpeg','mkv']
 
 # Get command line arguments and give help
 parser = argparse.ArgumentParser(description='This program will automatically rename subtitles to match video filenames so your mediaplayer automatically finds them. If you do not give it a directory with -d then it looks in the current directory. The program looks for the video extensions {0}, and the subtitle extensions {1} if it is not told to be more specific with -V and -S options.'.format(VID_EXTS,SUB_EXTS))
 
-parser.add_argument('--directory','-d',help='Directory to edit filenames.')
-parser.add_argument('--video','-V',help='Give video extension to only find videos with this exact extension.')
-parser.add_argument('--sub','-S',help='Give subs extension to only rename subs with this exact extension.')
+parser.add_argument('--directory','-d',help='Directory to edit filenames.',default=os.getcwd())
+parser.add_argument('--video','-V',help='Give video extension to only find videos with this exact extension.',default=VID_EXTS)
+parser.add_argument('--sub','-S',help='Give subs extension to only rename subs with this exact extension.',default=SUB_EXTS)
 parser.add_argument('--trial','-n',action='store_true',help='No action: print names of files to be renamed, but do not rename.')
-parser.add_argument('--verbose','-v',action='store_true',help='Verbose: print names of files that are successfully change.')
+parser.add_argument('--verbose','-v',action='store_true',help='Verbose: print names of files that are not changed and their season and episode numbers.')
+parser.add_argument('--quiet','-q',action='store_true',help='Be less verbose, i.e. hide filename changes.')
 
 args = parser.parse_args()
 
-# If a directory was given, then change to it
-if args.directory:
+# Print a newline to make it easier to read
+print()
+
+SUB_EXTS = args.sub
+VID_EXTS = args.video
+
+try:
     os.chdir(args.directory)
-directory_contents = os.listdir()
+except FileNotFoundError:
+    print('No such directory:',args.directory)
+    exit(1)
 
-# Find all the video files in the directory
-video_files = []
-for f in directory_contents:
-    srch = re.match(r'.*\.' + args.video if args.video else r'.*\.'+VID_EXTS,f)
-    if srch: video_files.append(srch.group(0))
+# Separate the subs and vids, put them in
+# separate dicts so they can be searched
+subtitles = dict()
+videos = dict()
+for f in os.listdir():
+    ep = Episode(f)
+    # If it cannot parse, then just move on
+    if not ep.episode:
+        continue
+    if ep.suffix:
+        if ep.suffix in SUB_EXTS:
+            subtitles.update({(ep.season,ep.episode):ep})
+        elif ep.suffix in VID_EXTS:
+            videos.update({(ep.season,ep.episode):ep})
 
-# regex to find subtitle episodes
-# The 2nd group is the season, and 4th group is the episode
-r0 = re.compile(r'.*(S|Season|season|s)([0-9]{2}).*(E|Episode|episode|e)([0-9]{2}).*\.' + (args.sub if args.sub else SUB_EXTS))
+# Combine the matching subs and vids
+for elt in sorted(subtitles):
+    if elt in videos.keys():
+        subtitle = subtitles[elt]
+        video = videos[elt]
 
-for f in sorted(os.listdir()):
-    # Find an episode subtitle
-    episode = r0.search(f)
+        if not args.quiet:
+            print('{0} -> {1}'.format(subtitle.filename,subtitle.subtitle_rename(video)))
+        if not args.trial:
+            os.rename(subtitle.filename,video.filename)
 
-    if episode:
-        # Find the videos that share same episode and season number
-        video_file = [video_file for video_file in video_files if episode.group(4) in video_file if episode.group(2)]
-
-        # Rename them and tell user, but check if final_extension is given and rename using that, else rename it to extension of file
-        if len(video_file) == 1:
-            new_subname = re.sub(r'\.[a-z0-9]+',PurePath(f).suffix,video_file[0])
-            if args.verbose or args.trial:
-                print('{0} -> {1}'.format(f,new_subname))
-            if not args.trial:
-                os.rename(f,new_subname)
-        elif len(video_file) > 1:
-            print('ERROR: I found too many matching video files:')
-            [print(vid) for vid in video_file]
-            exit(1)
+    elif args.verbose:
+        print('Video for S{0}E{1} not found'.format(elt[0],elt[1]))
