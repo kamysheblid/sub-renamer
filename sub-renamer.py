f82961b74fe8aada9088d82dfad3b66c9c3e7ffc
kamysheblid <kamysheblid@gmail.com>
2020-09-23 22:33:56 -0400

    fixing error

--- sub-renamer.py
+++ sub-renamer.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description='This program will automatically re
 parser.add_argument('--directory','-d',help='Directory to edit filenames.',default=os.getcwd())
 parser.add_argument('--video','-V',help='Give video extension to only find videos with this exact extension.',default=VID_EXTS)
 parser.add_argument('--sub','-S',help='Give subs extension to only rename subs with this exact extension.',default=SUB_EXTS)
-parser.add_argument('--trial','-n',action='store_true',help='No action: print names of files to be renamed, but do not rename.')
+parser.add_argument('--trial','-n',action='store_true',help='No action: print names of files to be renamed, but do not rename.',default=False)
 parser.add_argument('--verbose','-v',action='store_true',help='Verbose: print names of files that are not changed and their season and episode numbers.')
 parser.add_argument('--quiet','-q',action='store_true',help='Be less verbose, i.e. hide filename changes.')
 
@@ -50,10 +50,13 @@ for elt in sorted(subtitles):
         subtitle = subtitles[elt]
         video = videos[elt]
 
+        new_sub_filename = subtitle.subtitle_rename(video)
+
         if not args.quiet:
-            print('{0} -> {1}'.format(subtitle.filename,subtitle.subtitle_rename(video)))
+            print('{0} -> {1}'.format(subtitle.filename,new_sub_filename))
         if not args.trial:
-            os.rename(subtitle.filename,video.filename)
+            print(new_sub_filename)
+            # os.rename(subtitle.filename,new_sub_filename)
 
     elif args.verbose:
         print('Video for S{0}E{1} not found'.format(elt[0],elt[1]))
