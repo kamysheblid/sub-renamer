ef2e232d1d226dd01a3f0f70e797563c0853b959
kamysheblid <kamysheblid@gmail.com>
2020-11-15 00:31:49 -0500

    fixed directory tree to match setup.py conventions

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 import setuptools
 
-with open("README.html","r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name = "sub-renamer",
     version = "0.0.1",
+    license = 'LICENSE',
     author = "Kamy Sheblid",
     author_email = "kamysheblid@gmail.com",
     description = "Automatically rename subtitles filenames to their matching video filenames.",
-    long_description = long_description,
+    long_description = open('README.html').read(),
     long_description_content_type = "text/html",
     url = "https://github.com/kamysheblid/sub-renamer",
     packages = setuptools.find_packages(),
+    # packages = ['episode'],
     classifiers = [
         "Topic::Utilies",],
     python_requires = '>=3.7',
+    scripts = ['sub-renamer.py'],
+    install_requires = [
+        'python-Levenshtein'],
     )
